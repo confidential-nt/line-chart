fe0c7f606d5ff1cf7b096d8ee145f9bd2b46fd7f
confidential-nt <youyoy689@gmail.com>
2021-05-09 07:26:12 +0900

    write read me

--- app.js
+++ app.js
@@ -9,15 +9,15 @@ const targetId = "#chart";
 const dataLength = 6;
 
 try {
-  // const data = getData(dataLength);
-  const data = [
-    { label: "tomato", value: 13 },
-    { label: "orange", value: 43 },
-    { label: "apple", value: 55 },
-    { label: "banana", value: 40 },
-    { label: "strawberry", value: 60 },
-    { label: "grapes", value: 73 },
-  ];
+  const data = getData(dataLength);
+  // const data = [
+  //   { label: "tomato", value: 13 },
+  //   { label: "orange", value: 43 },
+  //   { label: "apple", value: 55 },
+  //   { label: "banana", value: 40 },
+  //   { label: "strawberry", value: 60 },
+  //   { label: "grapes", value: 73 },
+  // ];
   const chart = new LineChart(canvasWidth, canvasHeight, data, targetId);
   colorChangeListener(chart);
 } catch (error) {
